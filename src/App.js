e37b6544b7329ca39f06898accc630d576d25692
David Luu <dluu.audeo@gmail.com>
2022-03-05 23:28:39 -0800

    created new pages and updated routes for app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import './styles/App.css';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
+import { Route, Router } from 'react-router-dom';
+import './styles/App.css';
 import "./components/Header/Header.css"
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Chat":
+        return <Chat />;
+      case "Profile":
+        return <Profile />;
+      case "Login":
+        return <Login />;
+      case "Signup":
+        return <Signup />;
+      case "Contact":
+        return <Contact />;
+      default:
+        return <Home />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  
   return (
     <div className="App">
-      <h1>The Red Paperclip  📎</h1>
-      
-      {/* Header */}
-      <Header />
+      <Router>
+        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+
+
+          <Route path="/">
+            <h1>Test HomePage</h1>
+          </Route>
+
+          <Route path="/chat">
+            <h1>Test Chat Page</h1>
+          </Route>
+
+      </Router>
       {/* item cards */}
       {/* buttons below cards */}
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
-import "./Header.css"
 import PersonIcon from '@mui/icons-material/Person';
 import MessageIcon from '@mui/icons-material/Message';
+import { IconButton } from "@material-ui/core";
+import RedPaperclip from './redPaperclip.png';
+import "./Header.css"
 
 export default function Header() {
     return (
-        // BEM 
         <div className="header">
-            <PersonIcon />
-            <h2>Header Page</h2>
-            <MessageIcon />
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large"/>
+            </IconButton>
+            <img 
+                className="header__redPaperclip" 
+                src={RedPaperclip} 
+                alt="Red Paperclip"/>
+            <IconButton>
+                <MessageIcon className="header__icon" fontSize="large"/>
+            </IconButton>
         </div>
     );
 }

e9a02f9ca553dcd6898435ca28cc33a360e8d01d
David Luu <dluu.audeo@gmail.com>
2022-03-06 09:00:04 -0800

    updated pages to all display page text when routed from app

--- src/App.js
+++ src/App.js
@@ -1,59 +1,34 @@
-import React, { useState } from 'react';
-import Header from './components/Header/Header';
-import { Route, Router } from 'react-router-dom';
-import Chat from './components/pages/Chat';
-import Profile from './components/pages/Profile';
-import Login from './components/pages/Login';
-import Signup from './components/pages/Signup';
-import Contact from './components/pages/Contact';
-import Home from './components/pages/Home';
-import './styles/App.css';
-import "./components/Header/Header.css"
+import React from "react";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./styles/App.css";
+import "./components/Header/Header.css";
+import Home from "./components/pages/Home";
+import Chat from "./components/pages/Chat";
+import Contact from "./components/pages/Contact";
+import Login from "./components/pages/Login/";
+import Profile from "./components/pages/Profile/";
+import Signup from "./components/pages/Signup";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
-  const renderPage = () => {
-    switch (currentPage) {
-      case "Chat":
-        return <Chat />;
-      case "Profile":
-        return <Profile />;
-      case "Login":
-        return <Login />;
-      case "Signup":
-        return <Signup />;
-      case "Contact":
-        return <Contact />;
-      default:
-        return <Home />;
-    }
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
     <div className="App">
       <Router>
-        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-
-
-          <Route path="/">
-            <h1>Test HomePage</h1>
-          </Route>
-
-          <Route path="/chat">
-            <h1>Test Chat Page</h1>
-          </Route>
-
+      <Header />
+        <Routes>
+          <Route path="/red-paperclip/home" element={<Home />} />
+          <Route path="/red-paperclip/chat" element={<Chat />} />
+          <Route path="/red-paperclip/contact" element={<Contact />} />
+          <Route path="/red-paperclip/login" element={<Login />} />
+          <Route path="/red-paperclip/profile" element={<Profile />} />
+          <Route path="/red-paperclip/signup" element={<Signup />} />
+        </Routes>
       </Router>
       {/* item cards */}
       {/* buttons below cards */}
 
       {/* chat screen */}
       {/* individual chat screen */}
-
-
     </div>
   );
 }

05a16e5cdb0442f2d4600aa993804f2c7bbca646
David Luu <dluu.audeo@gmail.com>
2022-04-14 10:42:18 -0700

    connected firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase;'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgFi1WtoQjBoBdXZWH4Q2OSAZY7SZWnxw",
+    authDomain: "redpaperclip-4950d.firebaseapp.com",
+    projectId: "redpaperclip-4950d",
+    storageBucket: "redpaperclip-4950d.appspot.com",
+    messagingSenderId: "432460632446",
+    appId: "1:432460632446:web:60f51ec78164da8836e32f",
+    measurementId: "G-8VXSVHH44C"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db

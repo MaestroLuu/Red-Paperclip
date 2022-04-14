fd9ba000d66ef6b2b05878c3db352c67ad82bbf8
David Luu <dluu.audeo@gmail.com>
2022-04-14 11:21:52 -0700

    issue with webpack/browser; need to research for solution

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
 import "../styles/ItemCard.css";
+import db from "../firebase";
 
 export default function ItemCard() {
     const [items, setItems] = useState([
@@ -13,6 +14,12 @@ export default function ItemCard() {
             url: "https://media.cnn.com/api/v1/images/stellar/prod/211102125834-candle-homesickjustmarried.jpg?q=x_0,y_0,h_956,w_1698,c_fill/h_270,w_480"
         }
     ]);
+
+    useEffect(() => {
+        db.collection("items").onSnapshot(snapshot => (
+            setItems(snapshot.docs.map(doc => doc.data()))
+        ))
+    }, []);
 
     return (
         <div>       
